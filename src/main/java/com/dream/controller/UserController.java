f226afffffb10ce74176eacf2b9b0c43ae54c2a5
yoon <llsee01@naver.com>
2024-11-07 14:46:22 +0900

    세션확인 요청함수 추가

--- src/main/java/com/dream/controller/UserController.java
+++ src/main/java/com/dream/controller/UserController.java
@@ -1,4 +1,4 @@
- package com.dream.controller;
+package com.dream.controller;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -16,55 +16,74 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class UserController {
-	
-	 @Autowired
-	 private UserService userService;
+
+	@Autowired
+	private UserService userService;
+
+	// 세션정보 반환
+	@PostMapping("/userSessionInfo")
+	public ResponseEntity<Map<String, Object>> getUserSessionInfo(HttpSession session) {
+		Map<String, Object> sessionInfo = new HashMap<>();
+
+		// 세션에서 정보 가져오기
+		String userToken = (String) session.getAttribute("userToken");
+
+		if (userToken != null) {
+			sessionInfo.put("userToken", userToken);
+			return ResponseEntity.ok(sessionInfo);
+		} else {
+			sessionInfo.put("error", "No session found");
+			return ResponseEntity.status(403).body(sessionInfo);
+		}
+	}
+
 //	 회원 가입
 	@PostMapping("/userJoin")
-	public  ResponseEntity<Map<String, Object>> userJoin(@RequestBody Tb_User user){
+	public ResponseEntity<Map<String, Object>> userJoin(@RequestBody Tb_User user) {
 		// Vue js에서 보낸 데이터 처리
 //		System.out.println("Vue에서 받은 Post 데이터 : " + user);
-		
+
 		Map<String, Object> result = new HashMap<>();
-		
-		//비밀번호 암호화 인코딩
-		String hashedUserPw= userService.encryptedPassword(user.getPassword());
+
+		// 비밀번호 암호화 인코딩
+		String hashedUserPw = userService.encryptedPassword(user.getPassword());
 		user.setPassword(hashedUserPw);
-		
-		//회원가입
+
+		// 회원가입
 		userService.UserJoin(user);
-		
-		//회원정보 리턴
+
+		// 회원정보 리턴
 		result.put("email", user.getEmail());
 		result.put("memberType", user.getUserCategory());
 		return ResponseEntity.ok(result);
 	}
-	//로그인
+
+	// 로그인
 	@PostMapping("/userLogin")
 	public ResponseEntity<Map<String, Object>> userLogin(@RequestBody Tb_User user, HttpSession session) {
-		
+
 		Map<String, Object> result = new HashMap<>();
-		
+
 		String email = user.getEmail();
 		String password = user.getPassword();
 		System.out.println("vue 에서 받은 이메일 : " + email);
-		System.out.println("vue 에서 받은 비번 : " +password);
+		System.out.println("vue 에서 받은 비번 : " + password);
 		boolean checkPassword = userService.checkPassword(email, password);
 //		System.out.println(checkPassword);
-		if(checkPassword==true) {
-			 Tb_Token token =  userService.getToken(email);
+		if (checkPassword == true) {
+			Tb_Token token = userService.getToken(email);
 //			 System.out.println(token);
-			 
-			 // 토큰 인증 만료여부체크
-			 if(token.getExpireDate().isAfter(LocalDateTime.now())){
-				 session.setAttribute("UserToken",token.getUserToken());
-			 }else {
-				 
-			 } 
+
+			// 토큰 인증 만료여부체크
+			if (token.getExpireDate().isAfter(LocalDateTime.now())) {
+				session.setAttribute("userToken", token.getUserToken());
+			} else {
+
+			}
 		}
-		
-		Tb_User userInfo = userService.getUserInfo(email); 
-		
+
+		Tb_User userInfo = userService.getUserInfo(email);
+
 		result.put("name", userInfo.getName());
 		result.put("email", userInfo.getEmail());
 		result.put("userCate", userInfo.getUserCategory());
@@ -73,7 +92,3 @@ public class UserController {
 		return ResponseEntity.ok(result);
 	}
 }
-
-
-
-
