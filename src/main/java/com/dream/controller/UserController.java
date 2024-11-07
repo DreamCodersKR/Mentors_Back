4bd7c0e23b4b0499a7a7175ac6f8b2e5b012cfde
Enoch <lshllsee@naver.com>
2024-11-07 09:49:28 +0900

    enoch:login 2024.11.07

--- src/main/java/com/dream/controller/UserController.java
+++ src/main/java/com/dream/controller/UserController.java
@@ -1,5 +1,6 @@
  package com.dream.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,7 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dream.entity.Tb_Token;
 import com.dream.entity.Tb_User;
+import com.dream.mappers.UserMapper;
 import com.dream.service.UserService;
 import jakarta.servlet.http.HttpSession;
 
@@ -17,12 +21,11 @@ public class UserController {
 	
 	    @Autowired
 	    private UserService userService;
-	    
 //	 회원 가입
 	@PostMapping("/UserJoin")
 	public  ResponseEntity<Map<String, Object>> UserJoin(@RequestBody Tb_User user){
 		// Vue js에서 보낸 데이터 처리
-		System.out.println("Vue에서 받은 Post 데이터 : " + user);
+//		System.out.println("Vue에서 받은 Post 데이터 : " + user);
 		
 		Map<String, Object> result = new HashMap<>();
 		
@@ -40,18 +43,36 @@ public class UserController {
 	}
 	//로그인
 	@PostMapping("/UserLogin")
-	public String UserLogin(@RequestBody Tb_User user, HttpSession session) {
+	public ResponseEntity<Map<String, Object>> UserLogin(@RequestBody Tb_User user, HttpSession session) {
+		
+		Map<String, Object> result = new HashMap<>();
 		
 		String email = user.getEmail();
 		String password = user.getPassword();
-		System.out.println(email);
-		System.out.println(password);
+//		System.out.println(email);
+//		System.out.println(password);
 		boolean checkPassword = userService.checkPassword(email, password);
-		System.out.println(checkPassword);
+//		System.out.println(checkPassword);
+		if(checkPassword==true) {
+			 Tb_Token token =  userService.GetToken(email);
+			 System.out.println(token);
+			 
+			 // 토큰 인증 만료여부체크
+			 if(token.getExpireDate().isAfter(LocalDateTime.now())){
+				 session.setAttribute("UserToken",token.getUserToken());
+			 }else {
+				 
+			 }
+		}
 		
+		Tb_User userInfo = userService.GetUserInfo(email); 
+		
+		result.put("name", userInfo.getName());
+		result.put("email", userInfo.getEmail());
+		result.put("userCate", userInfo.getUserCategory());
+		result.put("mentorYn", userInfo.getMentorYn());
 
-		
-		return "dd";
+		return ResponseEntity.ok(result);
 	}
 }
 
